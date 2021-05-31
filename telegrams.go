a0a50a25202aa6f282847e8630728ec32d5e5d1b
justincaseof <justin.caseof@gmail.com>
2021-05-31 22:27:41 +0200

    reworked (and checked) some values

--- telegrams.go
+++ telegrams.go
@@ -20,34 +20,41 @@ type RunDatenResponsePayload []byte
 func (data RunDatenResponsePayload) parse(responsebytes []byte) RunDaten {
 	var result RunDaten
 
-	result.Program = int(responsebytes[2])
-	result.Person = int(responsebytes[3])
+	result.Program = int(responsebytes[0])
+	result.Person = int(responsebytes[1])
 
-	cycleState := responsebytes[4]
+	cycleState := responsebytes[2]
 	result.Cycling = cycleState > 0
 	if result.Cycling {
 		// anything?
 	}
 
+	// check: OK
 	result.PowerWatt = 0
-	powerRaw := responsebytes[5]
+	powerRaw := responsebytes[3]
 	if powerRaw >= 5 && powerRaw <= 80 || powerRaw >= 10 && powerRaw <= 160 { // validation
 		result.PowerWatt = int(powerRaw) * 5
 	}
 
-	result.RPM = int(responsebytes[6])
+	// check: OK
+	result.RPM = int(responsebytes[4])
 
-	result.DistanceMeters = int(responsebytes[7]) + int(responsebytes[8])*100
+	// check: OK
+	result.SpeedKMH = int(responsebytes[5])
 
-	result.PedalingTimeSeconds = int(responsebytes[9]) + int(responsebytes[10])
+	result.DistanceMeters = (int(responsebytes[6])<<8 + int(responsebytes[7])) * 100
 
-	result.EnergyJoules = int(responsebytes[11]) + int(responsebytes[12])*100
+	result.PedalingTimeSeconds = int(responsebytes[8])<<8 + int(responsebytes[9])
 
-	result.Pulse = int(responsebytes[13])
+	result.EnergyJoules = int(responsebytes[10])<<8 + int(responsebytes[11])
 
-	result.Gear = int(responsebytes[15])
+	// check: OK
+	result.Pulse = int(responsebytes[12])
+	//result.PulseState = int(responsebytes[13])
 
-	result.EnergyJoulesReal = int(responsebytes[16]) + int(responsebytes[17])*100
+	result.Gear = int(responsebytes[14])
+
+	result.EnergyJoulesReal = int(responsebytes[15]) + int(responsebytes[16])*100
 
 	return result
 }
